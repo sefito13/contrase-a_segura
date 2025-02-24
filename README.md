918920f28de1897bfa0f624aa07b9534c1fc31bf
Sebastian Eduardo Fandiño Orjuela <sebastianfandino13@gmail.com>
2025-02-24 18:44:00 -0500

    carga inicial, generador de contraseñas y instrucciones README

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# contrase-a_segura
-contraseña segura
+# Contraseña_segura
+Programa diseñado para practicar donde se realiza un programa donde se realizan generacion de contraseñas seguras.
+
+# Intrucciones
+- Comienza con un menu donde de pide si quieres tu contraseña con o sin caracteres especiales 
+    - 1. numeros y letras 
+    - 2. con caracteres especiales
+- Despues de esto te pedira la longitud de la contraseña 
+- Seguido de eso te pedira cuantas contraseñas quieres generar
+- Si te gusta alguna podras elegirla escribiendo el numero, si no solo enter para salir
+- Si escoges alguna te mostrar el resultado de la contraseña seleccionada
---
+++ generador_contraseñas.py
@@ -0,0 +1,63 @@
+import random
+import string
+
+numeros = string.digits
+letras_minu = string.ascii_lowercase
+letras_mayu = string.ascii_uppercase
+caracteres = "@.,#+*-"
+letras_numeros = numeros + letras_minu + letras_mayu
+todos_los_caracteres = numeros + letras_minu + letras_mayu + caracteres
+
+while True:
+
+    print("\n🔐 Escoje una opcion sobre que tipo de contraseña deseas 🎫")
+    print("1. Solo letras y numeros en la contraseña")
+    print("2. Incluir caracteres a la contraseña")
+
+    opcion = input("\nEscoje una opcion: ")
+
+    if opcion not in ['1', '2']:
+         print("❌ Escoje una opcion valida")
+         continue
+
+    try:
+        longitud_contra = int(input("Ingresa la longitud de la contraseña: "))
+        if longitud_contra <= 0:
+            print("❌ La longitud debe ser mayor a 0")
+            continue
+
+        cantidad_contraseñas = int(input("Ingresa cuantas contraseñas quieres: "))
+        if cantidad_contraseñas <= 0:
+            print("❌ La cantidad debe ser mayor a 0")
+     
+    except ValueError:
+          print("❌ Ingresa un numero valido")
+          continue
+
+    contraseñas = []
+    for _ in range(cantidad_contraseñas):
+        if opcion == '1':
+            contrasena = "".join(random.choices(letras_numeros, k= longitud_contra))
+        else:
+            contrasena = "".join(random.choices(todos_los_caracteres, k = longitud_contra))
+        contraseñas.append(contrasena)
+
+    print("\n🔑 Contraseñas generadas:")
+    for i, c in enumerate(contraseñas, start=1):
+        print(f"{i}. {c}")
+
+    while True:
+        seleccion = input("\n✏️ Escribe el número de la contraseña que deseas usar (o presiona Enter para salir): ")
+
+        if seleccion == "":
+            print("👋🏽 Hasta luego, ¡vuelve pronto!")
+            break
+
+        if seleccion.isdigit() and 1 <= int(seleccion) <= cantidad_contraseñas:
+            print(f"\n✅ Has seleccionado: **{contraseñas[int(seleccion)-1]}** 🔒")
+            break
+        else:
+            print("❌ Número inválido, intenta de nuevo.")
+
+    break
+   
